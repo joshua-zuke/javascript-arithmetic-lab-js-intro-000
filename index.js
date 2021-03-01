978b36581e5e3f636335875d7346e6ac38adf1f5
Joshua Zuke <zuke.joshua@gmail.com>
2021-03-01 23:41:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 function add(){
   number += 1
 }
+
+function subtract(){
+  number -= 1
+}
+
+function multiply(){
+  number *= 1
+}
+
+function divide(){
+  number /= 1
+}
+
+function add(a,b){
+  return (a+b)
+}
+
+function subtract(a,b){
+  return (a-b)
+}
+
+function divide(a,b){
+  return (a/b)
+}
+
+function multiply (a,b){
+  return (a*b)
+}
